2d8068b913637b436b050d9f18f500ca1b87abe4
AbdullahNezami <64733946+AbdullahNezami@users.noreply.github.com>
2020-05-03 18:42:30 +0500

    Added Some Empty Functions
    Soon they will get their code

--- LUDO/Ludo.cpp
+++ LUDO/Ludo.cpp
@@ -1,1 +1,29 @@
 #include "Ludo.h"
+void Ludo::ChangeTurn()
+{
+
+}
+void Ludo::PrintTurnMsg()
+{
+
+}
+void Ludo::Rolldice()
+{
+
+}
+bool Ludo::IsValidSelection()
+{
+	return true;
+}
+bool Ludo::IsValidDestination()
+{
+	return true;
+}
+bool Ludo::IsVacantSpot()
+{
+	return true;
+}
+void Ludo::RemovePlayer()
+{
+
+}
