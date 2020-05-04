472b7f725ae721326ec1cd36eb70b4406e7a58f1
bscs19014@itu.edu.pk <bscs19014@itu.edu.pk>
2020-05-04 23:22:02 +0500

    Piece Class added

---
+++ LUDO/Piece.h
@@ -0,0 +1,16 @@
+#pragma once
+class Piece
+{
+protected:
+	int pos;
+	bool hasKilled;
+	bool hasHomed;
+	bool isOut;
+	//bool isPaired;
+	bool isSafe;
+	bool isSelected;
+	//bool isKilled;
+public:
+	Piece() :hasKilled(false), hasHomed(false), isOut(false), isSafe(true), pos(0), isSelected(false) {}//0 means the piece is not out yet
+
+};
