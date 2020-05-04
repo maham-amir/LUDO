1fea300a7b8a368bb3de3a1bbd8705af6e95ffae
AbdullahNezami <64733946+AbdullahNezami@users.noreply.github.com>
2020-05-04 15:13:58 +0500

    Update Ludo.h

--- LUDO/Ludo.h
+++ LUDO/Ludo.h
@@ -12,20 +12,22 @@ class Ludo
 {
 	vector<Player*>Players;
 	int Plyturn;
-	vector <int>DiceRolls;
+	int DiceRolls;
 	vector<Player*>PlayersWon;
 	vector<Position>Stops;
 	vector<Box*>Boxes;
+	vector<Board**>B;
+	Position S, E;
 public:
 	void ChangeTurn();
 	void PrintTurnMsg();
 	void Rolldice();
+	void SelectPiece();
+	void SelectDestination();
 	bool IsValidSelection();
 	bool IsValidDestination();
 	bool IsVacantSpot();
 	void RemovePlayer();
-	Piece getSelectedPiece();
-	void play();
   	//code by BSCS19065
 	bool iskill();
 	void init(int);
